ffe95569ee61546b34a9c152c34f9b15adaa220a
kjoppke <krzysztof.joppke@gmail.com>
2020-05-10 21:09:21 +0200

    Zadanie 24137

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 4;
+let secondNumber = 20;
+
+function addition(firstNumber, secondNumber) { let result = firstNumber + secondNumber; return result; }
+
+let suma = addition(firstNumber, secondNumber);
+
+console.log(suma);
+
